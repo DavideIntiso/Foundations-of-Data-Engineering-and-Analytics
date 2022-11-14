d385492cf458b18a951008eec80ae6265654bec2
hindelma <ma.hindelang@campus.lmu.de>
2022-11-14 18:37:07 +0100

    changed output line

--- sheet3/Tweepy.py
+++ sheet3/Tweepy.py
@@ -3,7 +3,7 @@ import tweepy
 import csv
 
 #Put your Bearer Token in the parenthesis below
-client = tweepy.Client(bearer_token='YOUR BEARER TOKEN HERE')
+client = tweepy.Client(bearer_token='AAAAAAAAAAAAAAAAAAAAABLtjAEAAAAAATSEt6gf5Z%2FazcZbqr4L%2F92LXRo%3DaH6ZLVw7DB3OdM2EoVVqZv2k4OheB4GEOeBAy3RRiV1CuizbbP')
 
 
 # Get tweets that contain the word Innsbruck
@@ -16,7 +16,7 @@ file_name = 'tweets.csv'
 with open(file_name, 'a+') as filehandle:
     for tweet in tweepy.Paginator(client.search_recent_tweets, query=query,
                                   tweet_fields=['context_annotations', 'created_at'], max_results=100).flatten(
-            limit=10):
+            limit=50):
         response = client.get_liking_users(tweet.id)
         metadata = response.meta["result_count"]
-        filehandle.write('%s\n' % tweet.id + ',' + str(metadata))
+        filehandle.write('%s' % tweet.id + ',' + str(metadata) + '\n')
